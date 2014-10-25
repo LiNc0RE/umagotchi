0aa0355aad52a6b71ffeaa9484ab6a0e31e1402c
blurryroots <blurryroots@posteo.de>
2014-10-26 00:52:33 +0200

    project setup

---
+++ android/src/de/hawhamburg/se2/umagotchi/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.hawhamburg.se2.umagotchi.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.hawhamburg.se2.umagotchi.UmagotchiGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UmagotchiGame(), config);
+	}
+}

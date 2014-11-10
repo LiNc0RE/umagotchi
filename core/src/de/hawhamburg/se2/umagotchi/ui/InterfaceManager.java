cf749e231d3fe96dc86631bffff5693f8e3db92b
blurryroots <blurryroots@posteo.de>
2014-11-10 12:03:24 +0100

    outline for interface manager
    should take responsibility for checking
    user interaction with interface elements

---
+++ core/src/de/hawhamburg/se2/umagotchi/ui/InterfaceManager.java
@@ -0,0 +1,16 @@
+package de.hawhamburg.se2.umagotchi.ui;
+
+import de.hawhamburg.se2.umagotchi.UmagotchiGame;
+
+public
+class InterfaceManager {
+
+	private
+	UmagotchiGame game;
+	
+	public
+	InterfaceManager (UmagotchiGame game) {
+		this.game = game;
+	}
+	
+}
